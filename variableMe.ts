40c9f2aed12cfb7ca4c5f5532e9a0717efd236ea
Laura Vital Cacique <93387750+lauravitalc@users.noreply.github.com>
2023-10-31 10:40:06 -0300

    8 first classes from Learn Typescript

---
+++ variableMe.ts
@@ -0,0 +1,37 @@
+let greetings: string = "Hello Laura";
+console.log(greetings);
+
+// number
+let userId: number = 334455;
+
+// boolean
+let isLoggedIn: boolean = false;
+
+// function getValue(myVal: number): boolean {
+//     if(myVal > 5){
+//         return true
+//     }
+//     return "200 ok"
+// }
+
+const getHello = (s: string):string => {
+    return ""
+}
+
+const heros = ['Homelander', "Black Noir", "Starlight"]
+// const heros = [1, 2, 3]
+
+heros.map((hero): string => {
+    return `hero is ${hero}` 
+})
+
+function consoleError(errormsg: string): void{
+    console.log(errormsg);
+}
+
+function handleError(errormsg: string): never{
+    throw new Error(errormsg);
+}
+
+
+export {}
