8fe7271d798a146df1c143c470abfb02844d6779
Laura Vital Cacique <93387750+lauravitalc@users.noreply.github.com>
2023-11-13 10:47:31 -0300

    Tuples and Enums

---
+++ myEnums.ts
@@ -0,0 +1,8 @@
+enum SeatChoice {
+    AISLE = 10,
+    MIDDLE = "middle",
+    WINDOW,
+    FOURTH
+}
+
+const hcSeat = SeatChoice.AISLE
---
+++ myTupEnums.ts
@@ -0,0 +1,12 @@
+//const user: (string | number)[] = [1, "lc"];
+
+// tuples
+let tUser: [string, number, boolean]
+tUser = ["lc", 2000, true];
+// tUser = [true, 323, "lc"] WRONG!!!
+
+let rgb: [number, number, number] = [255,255,255];
+
+type User123 = [number, string]
+
+const newUser123: User123 = [122, "lalala@gmail.com"];
