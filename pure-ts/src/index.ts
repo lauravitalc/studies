83f85964b7db09b65eeb247e90dc0ae1167481e4
Laura Vital Cacique <93387750+lauravitalc@users.noreply.github.com>
2023-11-21 18:40:41 -0300

    Public, private, set, get, protected

--- pure-ts/src/index.ts
+++ pure-ts/src/index.ts
@@ -1,10 +1,49 @@
+// class User {
+//     public email: string 
+//     name: string // automatically public if you don't specify
+//     private readonly city: string = 'Belo Horizonte'
+//     constructor(email: string, name: string){
+//         this.email = email;
+//         this.name = name;
+//     }
+// }
+
 class User {
-    email: string 
-    name: string
-    constructor(email: string, name: string){
-        this.email = email;
-        this.name = name;
+    protected _courseCount = 1
+    readonly city: string = 'Belo Horizonte'
+    constructor(
+        public email: string, 
+        public name: string,
+        // private userId: string
+    ){}
+    
+    private deleteToken(){
+        console.log('Token deleted')
     }
+
+    get getAppleEmail(): string{
+        return `apple${this.email}`
+    }
+
+    get courseCount(): number {
+        return this._courseCount
+    }
+
+    set courseCount(courseNum): void {
+        if(courseNum <= 1){
+            throw new Error("Course count should be greater than 1")
+        }
+        this._courseCount = courseNum
+    }
+
 }
 
+class SubUser extends User {
+    isFamily: boolean = true
+    changeCourseCount(){
+        this._courseCount = 4
+    }
+}
 const laura = new User('laura@gmail.com','laura');
+
+// laura.deleteToken()
